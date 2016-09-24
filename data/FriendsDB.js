bba90b40a56eb66913d9ce8a93f6fac545d0cc68
Ronny Tomasetti <ronny.tomasetti@gmail.com>
2016-09-24 18:09:02 -0400

    Completed survey results page template.

--- data/FriendsDB.js
+++ data/FriendsDB.js
@@ -1,18 +1,69 @@
 var db = [
 	{
-		name   : 'Ronny',
-		photo  : 'https://dl.dropboxusercontent.com/u/2317062/profile_placeholder.jpg',
+		name   : 'Darth Vader',
+		photo  : 'https://dl.dropboxusercontent.com/u/2317062/FriendFinder/darth.jpg',
 		scores : [
 			'2',
+			'1',
+			'3',
+			'2',
+			'4',
+			'3',
+			'2',
+			'1',
+			'2',
+			'2'
+		],
+		uuid  : 'ithbsfljq66qh'
+	},
+	{
+		name   : 'Kylo Ren',
+		photo  : 'https://dl.dropboxusercontent.com/u/2317062/FriendFinder/kylo.png',
+		scores : [
+			'1',
+			'1',
+			'1',
+			'2',
+			'2',
+			'1',
+			'2',
+			'2',
+			'2',
+			'1'
+		],
+		uuid  : 'ithbsfljq66qh'
+	},
+	{
+		name   : 'Luke',
+		photo  : 'https://dl.dropboxusercontent.com/u/2317062/FriendFinder/luke.jpg',
+		scores : [
+			'4',
+			'5',
+			'3',
+			'4',
+			'3',
+			'4',
+			'3',
+			'2',
+			'4',
+			'5'
+		],
+		uuid  : 'ithbsfljq66qh'
+	},
+	{
+		name   : 'Master Yoda',
+		photo  : 'https://dl.dropboxusercontent.com/u/2317062/FriendFinder/yoda.jpeg',
+		scores : [
+			'5',
+			'4',
+			'4',
 			'3',
 			'4',
 			'5',
-			'1',
-			'2',
-			'3',
-			'2',
+			'5',
+			'5',
 			'4',
-			'4'
+			'3'
 		],
 		uuid  : 'ithbsfljq66qh'
 	}
